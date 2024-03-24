6e95e8dfa20a92db27fe2b3647f2f3ba4a7d3502
AstikSharma <132981717+AstikSharma@users.noreply.github.com>
2024-03-24 14:47:24 +0530

    Keeper app commit.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+var date = new Date();
+    var currentYear = date.getFullYear();
+return <footer><p>
+        Copyright {currentYear}
+    </p>
+    </footer>
+}
+
+export default Footer;
